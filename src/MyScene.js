3e09be8856530d1131c06feaaa5512607eee87ec
Drew McGowan <michael.mcgowan@students.makeschool.com>
2019-08-06 13:36:17 -0700

    bombs working

--- src/MyScene.js
+++ src/MyScene.js
@@ -4,9 +4,11 @@ import { config } from './config'
 class MyScene extends Scene {
     constructor() {
         super()
-        this.loopCount = 0
+        this.score = 0
+        this.gameOver = false
     }
-
+    // ======================================================
+    // preload
     preload ()
     {
         this.load.image('sky', '../assets/sky.png');
@@ -18,13 +20,19 @@ class MyScene extends Scene {
             { frameWidth: 32, frameHeight: 48 }
         );
     }
-
+    // =========================================================
+    // Create
     create() {
         const sky = this.add.image(0, 0, 'sky');
         sky.setOrigin(0, 0)
 
         this.createPlatforms();
         this.createPlayer();
+        this.createCursor();
+        this.createStars();
+        this.createBombs();
+    
+        this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
     }
 
     createPlatforms() {
@@ -33,6 +41,52 @@ class MyScene extends Scene {
         this.platforms.create(600, 400, 'ground');
         this.platforms.create(50, 250, 'ground');
         this.platforms.create(750, 220, 'ground');
+    }
+
+    createStars() {
+        this.stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: {  x: 12, y: 0, stepX: 70 }
+        });
+        
+        this.stars.children.iterate(function (child) {
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+        });
+        this.physics.add.collider(this.stars, this.platforms);
+        this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
+    }
+
+    collectStar(player, star) {
+        star.disableBody(true, true);
+        this.score += 10;
+        this.scoreText.setText('Score: ' + this.score);
+
+        if (this.stars.countActive(true) === 0) {
+            this.stars.children.iterate((child) => {
+                child.enableBody(true, child.x, 0, true, true);
+            });
+    
+            const x = (this.player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+            const bomb = this.bombs.create(x, 16, 'bomb');
+            bomb.setBounce(1);
+            bomb.setCollideWorldBounds(true);
+            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    
+        }
+    }
+
+    createBombs() {
+        this.bombs = this.physics.add.group();
+        this.physics.add.collider(this.bombs, this.platforms);
+        this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);
+    }
+
+    hitBomb(player, bomb) {
+        this.physics.pause();
+        player.setTint(0xff0000);
+        player.anims.play('turn');
+        this.gameOver = true;
     }
 
     createPlayer() {
@@ -60,6 +114,31 @@ class MyScene extends Scene {
             frameRate: 10,
             repeat: -1
         });
+    }
+    createCursor() {
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    // ===================================================
+    // update
+    update() {
+        if (this.cursors.left.isDown){
+            this.player.setVelocityX(-160);
+        
+            this.player.anims.play('left', true);
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+        
+            this.player.anims.play('right', true);
+        } else {
+            this.player.setVelocityX(0);
+        
+            this.player.anims.play('turn');
+        }
+    
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-330);
+        }
     }
 }
 
